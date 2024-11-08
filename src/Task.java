57da9ef66380bc113229f906864c5554003510d9
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-11-08 13:51:00 -0500

    Assignment Finished

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+
+    private String name;
+    private Priority priority;
+
+    public Task(String name, Priority priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public void displayDetails() {
+        System.out.print("Task: " + name);
+        System.out.println("|");
+        System.out.print("Priority" + priority);
+    }
 }
